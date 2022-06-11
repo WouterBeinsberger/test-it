cc18ca6cb9ef790eba7c03f494c7c0f48be06be1
Your Name <beinsbergerwouter@gmail.com>
2022-06-11 20:04:14 +0200

    feat: header added + added to app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,22 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-  </nav>
-  <router-view/>
+  <the-header></the-header>
+  <router-view></router-view>
 </template>
 
 <script>
+import TheHeader from '@/components/layouts/TheHeader.vue'
+
 export default {
-  
+  components: {
+    TheHeader
+  }
 }
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
+
+html {
+  font-family: 'Montserrat', sans-serif;
+}
+</style>
---
+++ src/components/layouts/TheHeader.vue
@@ -0,0 +1,60 @@
+<template>
+  <header>
+    <nav>
+      <ul class="nav-list">
+        <li class="nav-list__item">
+          <a href="#aboutUsSection">About Us</a>
+        </li>
+        <li class="nav-list__item">
+          <a href="#servicesSection">Services</a>
+        </li>
+        <li class="nav-list__item">
+          <a href="#contactSection">Contact</a>
+        </li>
+      </ul>
+      <ul class="nav-list">
+        <li class="nav-list__item">
+          <router-link to="/bugs">Bugs</router-link>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
+<style scoped>
+header {
+  height: 100px;
+  padding: 0rem 20rem 0rem 20rem;
+  display: flex;
+  align-items: center;
+}
+
+nav {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+}
+
+.nav-list {
+  border: 1px #ccc solid;
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  
+  display: flex;
+}
+
+.nav-list__item {
+  background: #ccc;
+  padding: 1rem 3rem;
+}
+
+.nav-list__item:hover{
+  background: rgb(129, 129, 129);
+}
+
+a {
+  text-decoration: none;
+  color: white;
+}
+</style>
