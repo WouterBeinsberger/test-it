640ace26f2e3b68179212b28dbf19f35cb8f0e85
Your Name <beinsbergerwouter@gmail.com>
2022-06-13 22:32:30 +0200

    refactor: add css vars

--- src/App.vue
+++ src/App.vue
@@ -14,9 +14,29 @@ export default {
 </script>
 
 <style>
-@import url('https://fonts.googleapis.com/css2?family=Montserrat&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Fira+Code:wght@300&family=Montserrat&display=swap');
+
+* {
+  margin: 0;
+}
 
 html {
-  font-family: 'Montserrat', sans-serif;
+  font-family: var(--font-montserrat);
+  
+}
+
+body {
+  background-color: var(--oxford-blue);
+}
+
+:root {
+  --oxford-blue: #112240ff;
+  --bdazzled-blue: #465a7bff;
+  --periwinkle-crayola: #bfcde0ff;
+  --white: #fefcfdff;
+  --spring-green: #37ff8bff;
+  --font-montserrat: 'Montserrat', sans-serif;
+  --font-fira-code: 'Fira Code', monospace;
+  --padding-normal: 0rem 15rem 0rem 15rem;
 }
 </style>
