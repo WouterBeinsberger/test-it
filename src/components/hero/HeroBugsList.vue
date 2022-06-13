1a8e5ba391a13e8028df9157cae2588b9c40b740
Your Name <beinsbergerwouter@gmail.com>
2022-06-13 22:34:10 +0200

    feat: added hero bugs list + added to bugs view

---
+++ src/components/hero/HeroBugsList.vue
@@ -0,0 +1,5 @@
+<template>
+  <section>
+    <h1>Bugs</h1>
+  </section>
+</template>
--- src/views/BugsView.vue
+++ src/views/BugsView.vue
@@ -1,9 +1,13 @@
 <template>
-  <h1>Bugs</h1>
+  <hero-bugs-list></hero-bugs-list>
 </template>
 
 <script>
+import HeroBugsList from '@/components/hero/HeroBugsList.vue'
+
 export default {
-  
+  components: {
+    HeroBugsList
+  }
 }
 </script>
