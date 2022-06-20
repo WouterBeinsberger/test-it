6c0ab0a6c675c0c1986536a008bb0018adf0b1a9
Your Name <beinsbergerwouter@gmail.com>
2022-06-20 16:38:01 +0200

    feat: base button added + added to main

---
+++ src/components/ui/BaseButton.vue
@@ -0,0 +1,54 @@
+<template>
+  <button v-if="!link" class="button" :class="getClasses">
+    <slot></slot>
+  </button>
+  <router-link v-else :to="to" :class="getClasses">
+    <slot></slot>
+  </router-link>
+</template>
+
+<script>
+export default {
+  props: {
+    classes: {
+      type: String,
+      required: false,
+    },
+    link: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    to: {
+      type: String,
+      required: false,
+      default: "/",
+    },
+  },
+  computed: {
+    getClasses() {
+      return this.classes;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.button,
+a {
+  cursor: pointer;
+  font-size: var(--font-body-normal);
+  border: none;
+}
+
+.on {
+  padding: 0.35rem 1rem;
+  background-color: var(--periwinkle-crayola);
+}
+
+.off {
+  padding: 0.35rem 1rem;
+  background-color: var(--bdazzled-blue);
+  color: var(--periwinkle-crayola);
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import router from './router'
 import store from './store'
 import apiFactory from './api'
 
+import BaseButton from '@/components/ui/BaseButton.vue'
 
 const app = createApp(App);
 
@@ -12,5 +13,7 @@ app.config.globalProperties.api = apiFactory;
 
 app.use(router);
 app.use(store);
+
+app.component('base-button', BaseButton);
 
 app.mount('#app');
