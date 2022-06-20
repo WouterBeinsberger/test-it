6d6ec3ca9b640b64f067a7362cb454c135ca39c9
Your Name <beinsbergerwouter@gmail.com>
2022-06-20 16:39:22 +0200

    feat: added update hero to actions

--- src/store/modules/hero/actions.js
+++ src/store/modules/hero/actions.js
@@ -4,5 +4,8 @@ export default {
   async loadHero(context) {
     const hero = await api.hero.getHero();
     context.commit('setHero', hero.data);
+  },
+  async updateHero(_, payload) {
+    await api.hero.updateHero(payload);
   }
 }
