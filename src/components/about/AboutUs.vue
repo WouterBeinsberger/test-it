090afb29d15635ccdd2431ed7677fa325eb06f23
Your Name <beinsbergerwouter@gmail.com>
2022-06-21 22:41:32 +0200

    feat(about): about us component created + added to home view

---
+++ src/components/about/AboutUs.vue
@@ -0,0 +1,27 @@
+<template>
+  <section v-if="aboutUs"></section>
+</template>
+
+<script>
+export default {
+  created() {
+    this.loadAboutUs();
+  },
+  computed: {
+    aboutUs() {
+      return this.$store.getters["about/aboutUs"];
+    },
+  },
+  methods: {
+    async loadAboutUs() {
+      try {
+        await this.$store.dispatch("about/loadAboutUs");
+      } catch (error) {
+        alert(
+          "something went wrong while fetching about us data: " + error.message
+        );
+      }
+    },
+  },
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,16 @@
 <template>
   <the-hero></the-hero>
+  <about-us></about-us>
 </template>
 
 <script>
-import TheHero from '@/components/hero/TheHero.vue'
+import TheHero from "@/components/hero/TheHero.vue";
+import AboutUs from "@/components/about/AboutUs.vue";
 
 export default {
   components: {
-    TheHero
-  }
-}
+    TheHero,
+    AboutUs,
+  },
+};
 </script>
