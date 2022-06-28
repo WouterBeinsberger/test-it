d54cd8aa96ccbda0d8182907fe90657f372ed53e
Your Name <beinsbergerwouter@gmail.com>
2022-06-28 19:26:31 +0200

    feat: fontawesome icons added

---
+++ src/assets/icons/index.js
@@ -0,0 +1,13 @@
+import { faCode } from "@fortawesome/free-solid-svg-icons/faCode";
+import { faBug } from "@fortawesome/free-solid-svg-icons/faBug";
+import { faGears } from "@fortawesome/free-solid-svg-icons/faGears";
+import { faCheck } from "@fortawesome/free-solid-svg-icons/faCheck";
+
+const icons = {
+  code: faCode,
+  bug: faBug,
+  gears: faGears,
+  check: faCheck,
+}
+
+export default icons
--- src/main.js
+++ src/main.js
@@ -8,6 +8,8 @@ import apiFactory from './api'
 import BaseButton from '@/components/ui/BaseButton.vue'
 import BaseBugsCard from '@/components/ui/BaseBugsCard.vue'
 
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
 const app = createApp(App);
 
 app.config.globalProperties.api = apiFactory;
@@ -17,5 +19,7 @@ app.use(store);
 
 app.component('base-button', BaseButton);
 app.component('base-bugs-card', BaseBugsCard);
+
+app.component('font-awesome-icon', FontAwesomeIcon);
 
 app.mount('#app');
