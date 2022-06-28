ef6f20fab5a3bd8b88c500e06b28abdc239d332f
Your Name <beinsbergerwouter@gmail.com>
2022-06-28 19:28:00 +0200

    feat(about): finished cards

--- src/components/about/AboutUs.vue
+++ src/components/about/AboutUs.vue
@@ -1,11 +1,43 @@
 <template>
-  <section v-if="aboutUs"></section>
+  <section
+    class="about-us"
+    v-if="aboutUs && aboutUs.content.section.isDisplayed"
+    :id="aboutUs.content.section.elementId"
+  >
+    <h1
+      class="about-us__title"
+      v-if="aboutUs.content.title.isDisplayed"
+      :id="aboutUs.content.title.elementId"
+    >
+      {{ aboutUs.content.title.value }}
+    </h1>
+    <div
+      class="about-us__list"
+      v-if="aboutUs.content.aboutUsItemList.isDisplayed"
+      :id="aboutUs.content.aboutUsItemList.elementId"
+    >
+      <template
+        v-for="aboutUsListItem in aboutUs.content.aboutUsItemList.values"
+        :key="aboutUsListItem.id"
+      >
+        <about-us-list-item-card
+          :aboutUsItem="aboutUsListItem"
+          :id="aboutUsListItem.elementId"
+        ></about-us-list-item-card>
+      </template>
+    </div>
+  </section>
 </template>
 
 <script>
+import AboutUsListItemCard from "./AboutUsListItemCard.vue";
+
 export default {
   created() {
     this.loadAboutUs();
+  },
+  components: {
+    AboutUsListItemCard,
   },
   computed: {
     aboutUs() {
@@ -25,3 +57,24 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.about-us {
+  padding: var(--padding-normal);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.about-us__title {
+  color: var(--gray-lightest);
+  font-size: var(--font-heading-normal);
+  padding-bottom: 75px;
+}
+
+.about-us__list {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+}
+</style>
---
+++ src/components/about/AboutUsListItemCard.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="about-us-list__item--card">
+    <font-awesome-icon
+      class="about-us-list-item__card--icon"
+      :icon="iconName"
+    ></font-awesome-icon>
+    <h2 class="about-us-list-item__card--title">
+      {{ aboutUsItemValue.itemTitle }}
+    </h2>
+  </div>
+</template>
+
+<script>
+import icons from "@/assets/icons";
+
+export default {
+  props: ["aboutUsItem"],
+  computed: {
+    aboutUsItemValue() {
+      return this.aboutUsItem;
+    },
+    iconName() {
+      return icons[this.aboutUsItemValue.icon];
+    },
+  },
+};
+</script>
+
+<style scoped>
+.about-us-list__item--card {
+  background-color: var(--gray-darker);
+  padding: 1.5rem 0rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  flex: 1;
+}
+
+.about-us-list__item--card:not(:first-child):not(:last-child) {
+  margin: 0px 5px;
+}
+
+.about-us-list__item--card:first-child {
+  margin-right: 5px;
+}
+
+.about-us-list__item--card:last-child {
+  margin-left: 5px;
+}
+
+.about-us-list-item__card--icon {
+  color: var(--blue);
+  font-size: var(--font-heading-large);
+  padding: 1.5rem;
+}
+
+.about-us-list-item__card--title {
+  color: var(--gray-light);
+  font-size: var(--font-sub-heading-normal);
+  padding-top: 1.5rem;
+}
+</style>
