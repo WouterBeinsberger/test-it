d2d85a3c2db376c91159bd6be090d4d581d7e265
Your Name <beinsbergerwouter@gmail.com>
2022-06-29 17:54:54 +0200

    servicesModule added to store index.js

--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,14 @@ import { createStore } from 'vuex'
 import headerModule from './modules/header/index.js'
 import heroModule from './modules/hero/index.js'
 import aboutModule from './modules/about/index.js'
+import servicesModule from './modules/services/index.js'
 
 const store = createStore({
   modules: {
     header: headerModule,
     hero: heroModule,
-    about: aboutModule
+    about: aboutModule,
+    services: servicesModule
   },
   state() {
     return {}
